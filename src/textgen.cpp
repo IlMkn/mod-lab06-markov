8a067624892b5faaae4383a75ce4692fd22b392b
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-04-24 01:36:31 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,4 +1,4 @@
-#include "textgen.h"
+// Copyright 2021 GHA Test Team
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,52 +6,43 @@
 #include <deque>
 #include <map>
 #include <algorithm>
+#include "textgen.h"
 
-textgen::textgen(std::string pathToSource, int prefixLength)
-{
+textgen::textgen(std::string pathToSource, int prefixLength) {
     ReadSourceFile(pathToSource);
-    for (int i = 0; i < tempWords.size() - NPREF; i++)
-    {
+    for (int i = 0; i < tempWords.size() - NPREF; i++) {
         prefix tempPref = GetPref(i);
 
         std::vector<std::string> tempSuff = GetSuff(tempPref);
 
         std::map<prefix, std::vector<std::string>>::iterator it = statetab.find(tempPref);
-        if (it == statetab.end())
-        {
+        if (it == statetab.end()) {
             statetab.insert(make_pair(tempPref, tempSuff));
         }
     }
 }
 
-textgen::prefix textgen::GetPref(int ind)
-{
+textgen::prefix textgen::GetPref(int ind) {
     prefix tempPref;
-    for (int j = 0; j < NPREF; j++)
-    {
+    for (int j = 0; j < NPREF; j++) {
         tempPref.push_back(tempWords[ind + j]);
     }
     return tempPref;
 }
 
-std::vector<std::string> textgen::GetSuff(prefix pref)
-{
+std::vector<std::string> textgen::GetSuff(prefix pref) {
     std::vector<std::string> suffix;
-    for (int i = 0; i < tempWords.size() - NPREF; i++)
-    {
+    for (int i = 0; i < tempWords.size() - NPREF; i++) {
         int tempSum = 0;
-        for (int j = 0; j < NPREF; j++)
-        {
-            if (pref[j] == tempWords[i + j])
-            {
+        for (int j = 0; j < NPREF; j++) {
+            if (pref[j] == tempWords[i + j]) {
                 tempSum++;
             }
         }
 
-        if (tempSum == NPREF)
-        {
-            if (std::find(suffix.begin(), suffix.end(), tempWords[i + NPREF]) == suffix.end())
-            {
+        if (tempSum == NPREF) {
+            if (std::find(suffix.begin(), suffix.end(), 
+                tempWords[i + NPREF]) == suffix.end()) {
                 suffix.push_back(tempWords[i + NPREF]);
             }
         }
@@ -59,16 +50,14 @@ std::vector<std::string> textgen::GetSuff(prefix pref)
     return suffix;
 }
 
-std::string textgen::GetCertainSuffix(textgen::prefix pref)
-{
+std::string textgen::GetCertainSuffix(textgen::prefix pref) {
     std::string result;
 
     result = statetab.at(pref)[rand() % statetab.at(pref).size()];
     return result;
 }
 
-std::string textgen::CreateText(int wordcount)
-{
+std::string textgen::CreateText(int wordcount) {
     std::string outputStr;
 
     int count = 0;
@@ -76,20 +65,17 @@ std::string textgen::CreateText(int wordcount)
 
     count = count + NPREF;
 
-    for (int i = 0; i < NPREF; i++)
-    {
+    for (int i = 0; i < NPREF; i++) {
         outputStr += firstPref[i] + " ";
     }
 
     prefix tempPref = firstPref;
 
-    while ((count < wordcount) && (GetSuff(tempPref).size() > 0))
-    {
+    while ((count < wordcount) && (GetSuff(tempPref).size() > 0)) {
         std::string tempStr = GetCertainSuffix(tempPref);
         outputStr += tempStr + " ";
         count++;
-        for (int i = 0; i < NPREF - 1; i++)
-        {
+        for (int i = 0; i < NPREF - 1; i++) {
             tempPref[i] = tempPref[i + 1];
         }
         tempPref[NPREF - 1] = tempStr;
@@ -98,8 +84,7 @@ std::string textgen::CreateText(int wordcount)
     return outputStr;
 }
 
-void textgen::ReadSourceFile(std::string pathToSourceText)
-{
+void textgen::ReadSourceFile(std::string pathToSourceText) {
     std::ifstream infile(pathToSourceText);
     std::string word;
     while (infile >> word)
